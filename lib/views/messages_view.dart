40a1db829e9a33b4204d34a980ddcb92783dea4c
vamshi krishna <113350510+dumbun@users.noreply.github.com>
2023-04-02 01:13:40 +0530

    day 3

---
+++ lib/views/messages_view.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import '../widgets/list_view.dart';
+
+Widget messagesView(homeMessageslist) {
+  return SizedBox(
+    height: 700.0,
+    child: homeMessageslist.isEmpty
+        ? Column(
+            children: [
+              const Text(
+                "No Chats Found",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+                width: double.infinity,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(
+                    Icons.edit_document,
+                    color: Colors.grey,
+                  ),
+                  Text(
+                    "Click the Icon to Start Texting",
+                    style: TextStyle(color: Colors.grey),
+                  )
+                ],
+              )
+            ],
+          )
+        : ListView.builder(
+            itemBuilder: (context, index) {
+              return Card(
+                elevation: 0.0,
+                child: ListMessagesView(
+                  image: homeMessageslist[index].image,
+                  contactName: homeMessageslist[index].name,
+                  lastMassages: homeMessageslist[index].lastMassages,
+                ),
+              );
+            },
+            itemCount: homeMessageslist.length,
+          ),
+  );
+}
